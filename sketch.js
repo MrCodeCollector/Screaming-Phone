4478b244be7485acd54a426b58549d0701b78493
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-06-11 16:51:07 -0700

    v0.2
    -fixed bogus mistakes

--- sketch.js
+++ sketch.js
@@ -3,11 +3,18 @@ var mild;
 var fuego;
 
 function setup() {
-  light = loadSound()
+  mild = loadSound('assets/mild.m4a');
+  
+  createCanvas(500, 500);
+  background(75);
 }
 
 function draw() {
   if(accelerationY > 0) {
-    
-  } else if(accelerationY > 10)
+    mild.play();
+    background(225);
+  } else if(accelerationY <= 10 && mild.isPlaying() ) {
+    mild.stop();
+    background(0);
+  }
 }
